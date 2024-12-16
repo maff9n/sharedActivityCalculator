b2090a7f92deaa25b8a800989136bff41eff107f
Marvin Mann <marvin1995mann@gmail.com>
2024-12-16 02:17:21 +0100

    bound evaluation to onEdit functionality, added comments, added user input checks

--- Code.js
+++ Code.js
@@ -1,70 +1,92 @@
-const CONSTANT = {
-  EVALUATION_SHEET : SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Evaluation"),
-  PARTICIPANTS_SHEET : SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Participants"),
-  EVENTS_SHEET : SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Events")
+const COLORS = {
+  red: "#f5c4c4",
+  orange: "#fddabc",
+  yellow: "#fff4cc",
+  green: "#cde5d2",
+  blue: "#b4d5e5",
+  indigo: "#d2c4e4",
 };
-  
-function createHeader() {
-  CONSTANT.EVALUATION_SHEET.getRange("A1:D1").merge()
-  .setValue("The overview below is solely based on the contents of sheet \"Events\" and \"Participants\".")
-  .setBackground("#B7B7B7")
-  .setWrap(true);
+// Todo: use COLORS to make participants in the evaluation overview more visually distinguishable
 
-  CONSTANT.EVALUATION_SHEET.autoResizeRows(1, 1);
+// the sheet "Participants" and the sheet "Events" are meant to capture user input by hand
+// the sheet "Evaluation" gets its content solely from calculaten based on meantioned user input
+const evaluationSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Evaluation");
+const participantsSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Participants");
+const eventsSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Events");
+
+// returns all participant names in an array with unique values
+// uniqueness is enforced by spreadsheet data validation functionality
+function getParticipants(){
+  const participantNames = participantsSheet
+    .getRange(2, 1, participantsSheet.getLastRow() - 1, 1)
+    .getValues()
+    .flat();
+  Logger.log("Participant names from the spreadsheet:\n" + participantNames);
+  return participantNames;
 }
 
-function getMatrix(){
-  const participantsSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Participants");
-  const range = participantsSheet.getRange(2, 1, participantsSheet.getLastRow() - 1, 1); // From A2 to the last row
-  
-  // Get the values in that range
-  const values = range.getValues();
-  
-  // Flatten the 2D array into a 1D array
-  let array = values.map(row => row[0]);
-  array = array.map(payer => {
-    let body = { name: payer };
-    const others = array.filter(other => other !== payer);
-    
-    others.forEach( other => {
-      body[other] = 0;
-    })
-    return body;
-    });
-  return array;
-}
-
+// returns all events data in a two dimensional array
+// each event consists of amount, payer, beneficiaries
 function getEvents(){
-  const eventsSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Events");
-  const lastRow = eventsSheet.getLastRow();
-  
-  // Erstelle eine Range ab der zweiten Zeile, die die ersten drei Spalten umfasst
-  const range = eventsSheet.getRange(2, 2, lastRow - 1, 3);  // Startet in Zeile 2, Spalte 1 (A), geht bis zur letzten Zeile und umfasst 3 Spalten
-  // const range = eventsSheet.getRange(2, 2, 10, 3);  // Startet in Zeile 2, Spalte 1 (A), geht bis zur letzten Zeile und umfasst 3 Spalten
-  
-  // Beispiel: Logge den Wert der Range (kann auch in anderen Operationen verwendet werden)
-  const values = range.getValues();
-  Logger.log("Log 0002");
-  Logger.log(values);
-  return values;
-
+  const events = eventsSheet
+    .getRange(2, 2, eventsSheet.getLastRow() - 1, 3)
+    .getValues();
+  Logger.log("Event details from the spreadsheet:\n" + events);
+  return events;
 }
 
-function run(matrix, payer, amount, people){
-  Logger.log("start run\npayer is " + payer + "\namount is " + amount + "\npeople are " + people);
-  // app := amount per person
-  people = people.split(', ').map(item => item.trim());
-  let app = amount/people.length;
+// return a matrix which is used to cope with all the data from the events and track who owns who and which amount 
+// a matrix inside this script got the following structure:
+//  [
+//    { name:"name1", name2:amount, name3:amount, ...},
+//    { name:"name2", name1:amount, name3:amount, ...},
+//    ...
+//  ]
+function getMatrix(participantNames){
+  const matrix = participantNames.map(payer => {
+    let matrixElement = { name: payer };
+    
+    const others = participantNames.filter(other => other !== payer);
+    others.forEach( other => {
+      matrixElement[other] = 0;
+    });
+    return matrixElement;
+    });
+  Logger.log("Initial participant matrix:\n" + matrix);
+  return matrix;
+}
+
+// loop with run() through all the events and fill the matrix with data while calculating everything
+function run(matrix, payer, amount, beneficiaries){
+
+  if(payer == null || payer == ""){
+    Logger.log("Variable payer inside run() holds no applicable value");
+    return matrix;
+  }
+    if(amount == null || amount == ""){
+    Logger.log("Variable amount inside run() holds no applicable value");
+    return matrix;
+  }
+    if(beneficiaries.length == 0){
+    Logger.log("Variable beneficiaries inside run() holds no applicable value");
+    return matrix;
+  }
+
+
+  Logger.log("The following details of one event are now added to the participant matrix:\npayer is " + payer + "\namount is " + amount + "\nbeneficiaries are " + beneficiaries);
+  
+  beneficiaries = beneficiaries.split(', ').map(item => item.trim());
+  let app = amount/beneficiaries.length;
   app = Math.round(app * 100)/100;
   Logger.log("Calculation for app is " + app);
 
-  const others = people.filter(other => other !== payer);
+  const others = beneficiaries.filter(other => other !== payer);
 
   return matrix.map( obj => {
     if( obj.name == payer ){
-      others.forEach(beneficiarie => {
-        obj[beneficiarie] = obj[beneficiarie] + app;
-        obj[beneficiarie] = Math.round(obj[beneficiarie] * 100) / 100;
+      others.forEach(beneficiary => {
+        obj[beneficiary] = obj[beneficiary] + app;
+        obj[beneficiary] = Math.round(obj[beneficiary] * 100) / 100;
       })
     }
 
@@ -76,25 +98,9 @@ function run(matrix, payer, amount, people){
   });
 }
 
-function startCalculation() { 
-  var eventsSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Events");
-  var monitoredColumns = [2, 3, 4];
-
-  createHeader();
-  let matrix = getMatrix();
-  const events = getEvents();
-  
-  let n = 0
-  events.forEach( event => {
-    Logger.log("Start Run #" + n);
-    Logger.log(matrix);
-    matrix = run(matrix, event[1], event[0], event[2]);
-    Logger.log("End Run #" + n);
-    n++;
-    Logger.log(matrix);
-  })
-
-  const output = [];
+// write all usefull data from the matrix to a range in the Evaluation sheet 
+function writeDataToEvaluationSheet(matrix){
+  let output = [];
   
   matrix.forEach( obj => {
     const name = obj.name;
@@ -114,7 +120,42 @@ function startCalculation() {
   Logger.log("Output would be the following...");
   Logger.log(output);
 
-  var evaluationSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Evaluation");
   evaluationSheet.getRange(2, 1, output.length, output[0].length).setValues(output);
+}
 
+// main
+function calcEvaluation(){
+  // collect user input from the sheet "Events" and the sheet "Participants"
+  const events = getEvents();
+  const participants = getParticipants();
+
+  // create initial matrix. Filled with participant names and zeroes.
+  let matrix = getMatrix(participants);
+  
+  // step by step use all events to derive the final matrix
+  // for each event there is a so called "run" which adds the events details to the matrix
+  let n = 0
+  events.forEach( event => {
+    Logger.log("Start Run #" + n);
+    Logger.log(matrix);
+    matrix = run(matrix, event[1], event[0], event[2]);
+    n++;
+  })
+
+  // write calculation result to the "Evaluation" sheet
+  writeDataToEvaluationSheet(matrix);
+}
+
+function onEdit(e) { 
+  const triggerSheet = e.source.getActiveSheet();
+  const triggerRange = e.range;
+  
+  const isTargetSheet = triggerSheet.getName() === eventsSheet.getName();
+  const isTargetColumn = triggerRange.getColumn() >= 2 && triggerRange.getColumn() <= 4;
+
+  // Check if an edit happened in sheet 'Events' and the edit occurred in columns B, C, or D
+  // If so calculate a complete evaluation
+  if (isTargetSheet && isTargetColumn) {
+    calcEvaluation(); 
+  }  
 }
